928b4aebf2b060a0d8e6233b227afd7fb9287b63
ew0345 <ew0345@users.noreply.github.com>
2023-06-24 07:46:46 -0400

    Major update
    Changes:
    - Rewrote entire script
    - Fixed bug with auto-checking/building traps
    - Added in-game event popup for certain pieces of information
    - Made Food/Water, Amount Healed, Base HP & Base Storage toggle instead of once & done
    - Added proper support for auto upgrading ship
    - Redid menu buttons to match the games appearance (both light & dark mode supported)
    
    Known Issues:
    - Anything that involving the world map will not update until something has been by the game to update the UI (HP, storage, potentially even inventory)
    
    Todo:
    - Added hotkeys for cheats back

--- adrCheats.js
+++ adrCheats.js
@@ -5,18 +5,29 @@ var CheatMain = {
         amountOfBaseHP: 999999,
         amountOfBagSpace: 999999,
         amountOfMedsToAdd: 100,
-        resources: ['alien alloy', 'bait', 'bullets', 'charm', 'cloth', 'coal', 'compass', 'cured meat', 'energy cell', 'fur', 'iron', 'leather', 'meat', 'medicine', 'scales', 'steel', 'teeth', 'torch', 'wood'],
+        resources: ['alien alloy', 'bait', 'bullets', 'charm', 'cloth', 'coal', 'compass', 'cured meat', 'energy cell', 'fur', 'iron', 'leather', 'meat', 'medicine', 'scales', 'steel', 'sulphur', 'teeth', 'torch', 'wood'],
         weapons: ['bayonet', 'bolas', 'bone spear', 'iron sword', 'steel sword', 'rifle', 'laser rifle', 'grenade'],
         perks: ['barbarian', 'boxer', 'desert rat', 'evasive', 'gastronome', 'martial artist', 'percise', 'scout', 'slow metabolism', 'stealthy', 'unarmed master'],
         automate: {
             stoke: false,
             gather: false,
-            check: false
+            check: false,
+            ship: false
         },
         stokeInterval: null,
         gatherInterval: null,
         trapInterval: null,
-        buildTrapInterval: null
+        buildTrapInterval: null,
+        healInterval: null,
+        shipInterval: null,
+        modifiedVars: {
+            MOVES_PER_FOOD: 0,
+            MOVES_PER_WATER: 0,
+            MEAT_HEAL: 0,
+            MEDS_HEAL: 0,
+            BASE_HEALTH: 0,
+            DEFAULT_BAG_SPACE: 0
+        }
     },
     adrStoke: function() {
         if ($SM.get('game.fire.value') == 0) lightButton.click();
@@ -30,11 +41,11 @@ var CheatMain = {
                 }
             }, 100);
             CheatMain.vars.automate.stoke = true;
-            Notifications.printMessage('Enabled Auto Fire Stoking');
+            Notifications.printMessage('Enabled Auto Fire Stoking.');
         } else if (CheatMain.vars.automate.stoke) {
             clearInterval(CheatMain.vars.stokeInterval);
             CheatMain.vars.automate.stoke = false;
-            Notifications.printMessage('Disabled Auto Fire Stoking');
+            Notifications.printMessage('Disabled Auto Fire Stoking.');
         }
     },
     adrGather: function() {
@@ -42,11 +53,11 @@ var CheatMain = {
             if (!CheatMain.vars.automate.gather) {
                 CheatMain.vars.gatherInterval = setInterval(function() { gatherButton.click(); }, 2000);
                 CheatMain.vars.automate.gather = true;
-                Notifications.printMessage("Enabled Auto Wood Gathering");
+                Notifications.printMessage("Enabled Auto Wood Gathering.");
             } else if (CheatMain.vars.automate.gather) {
                 clearInterval(CheatMain.vars.gatherInterval);
                 CheatMain.vars.automate.gather = false;
-                Notifications.printMessage("Disabled Auto Wood Gathering");
+                Notifications.printMessage("Disabled Auto Wood Gathering.");
             }
         } else {
             Events.startEvent({
@@ -101,33 +112,171 @@ var CheatMain = {
        }
     },
     adrGiveResources: function() {
-        return;
+        for (var i = 0; i < CheatMain.vars.resources.length; i++) {
+            $SM.add('stores.'+CheatMain.vars.resources[i]+'', CheatMain.vars.amountToAdd);
+            $SM.set('stores.compass', 1);
+        }
+
+        Notifications.printMessage('Gave '+CheatMain.vars.amountToAdd+' resources.');
     },
     adrGiveWeapons: function() {
-        return;
+        for (var i = 0; i < CheatMain.vars.weapons.length; i++) {
+            $SM.add('stores.'+CheatMain.vars.weapons[i]+'', CheatMain.vars.amountToAdd);
+        }
+
+        Notifications.printMessage('Gave '+CheatMain.vars.amountToAdd+' weapons.');
     },
     adrGiveAllPerks: function() {
-        return;
+        for (var i = 0; i < CheatMain.vars.perks.length; i++) {
+            $SM.set('character.perks["'+CheatMain.vars.perks[i]+'"]', true);
+        }
+
+        Notifications.printMessage('Gave All Perks.');
+    },
+    adrDustPathUnlocked: function() {
+        if ($SM.get('stores.compass') < 1 || $SM.get('stores.compass') === undefined) {
+            Events.startEvent({
+                title: _('Lacking Compass'),
+                scenes: {
+                    start: {
+                        text: [_('Dusty Path is not unlocked.'), _('A compass is requied.')],
+                        buttons: {
+                            'okay': {
+                                text: _('Okay'),
+                                nextScene: 'end'
+                            }
+                        }
+                    }
+                }
+            });
+            return false;
+        } else {
+            return true;
+        }
     },
     adrNoWaterOrFood: function() {
-        return;
+        if (CheatMain.adrDustPathUnlocked()) {
+            if (CheatMain.vars.modifiedVars.MOVES_PER_FOOD != World.MOVES_PER_FOOD && CheatMain.vars.modifiedVars.MOVES_PER_FOOD < $SM.MAX_STORE && CheatMain.vars.modifiedVars.MOVES_PER_FOOD != 0) {
+                World.MOVES_PER_FOOD = CheatMain.vars.modifiedVars.MOVES_PER_FOOD;
+                World.MOVES_PER_WATER = CheatMain.vars.modifiedVars.MOVES_PER_WATER;
+
+                Notifications.printMessage("Moves per food and water reset to original values.");
+            } else {
+                CheatMain.vars.modifiedVars.MOVES_PER_FOOD = World.MOVES_PER_FOOD;
+                CheatMain.vars.modifiedVars.MOVES_PER_WATER = World.MOVES_PER_WATER;
+                World.MOVES_PER_FOOD = $SM.MAX_STORE;
+                World.MOVES_PER_WATER = $SM.MAX_STORE;
+
+                Notifications.printMessage("Moves per food and water set to: "+$SM.MAX_STORE+".");
+            }
+        }
     },
     adrHighHealing: function() {
-        return;
+        if (CheatMain.adrDustPathUnlocked()) {
+            if (CheatMain.vars.modifiedVars.MEAT_HEAL != World.MEAT_HEAL && CheatMain.vars.modifiedVars.MEAT_HEAL < CheatMain.vars.amountToHeal && CheatMain.vars.modifiedVars.MEAT_HEAL != 0) {
+                World.MEAT_HEAL = CheatMain.vars.modifiedVars.MEAT_HEAL;
+                World.MEDS_HEAL = CheatMain.vars.modifiedVars.MEDS_HEAL;
+
+                clearInterval(CheatMain.vars.healInterval);
+
+                Notifications.printMessage("Heal values of Meat & Meds reset to original values.");
+            } else {
+                CheatMain.vars.modifiedVars.MEAT_HEAL = World.MEAT_HEAL;
+                CheatMain.vars.modifiedVars.MEDS_HEAL = World.MEDS_HEAL;
+                
+                // Seemed to reset under unknown circumstances so had to add this
+                CheatMain.vars.healInterval = setInterval(function () {
+                    World.MEAT_HEAL = CheatMain.vars.amountToHeal;
+                    World.MEDS_HEAL = CheatMain.vars.amountToHeal;
+                }, 1000);
+
+                Notifications.printMessage("Heal values of Meat & Meds set to: "+CheatMain.vars.amountToHeal+".");
+            }
+        }
     },
     adrLotsOfHP: function() {
-        return;
+        if (CheatMain.adrDustPathUnlocked()) {
+            if (CheatMain.vars.modifiedVars.BASE_HEALTH != World.BASE_HEALTH && CheatMain.vars.modifiedVars.BASE_HEALTH < CheatMain.vars.amountOfBaseHP && CheatMain.vars.modifiedVars.BASE_HEALTH != 0) {
+                World.BASE_HEALTH = CheatMain.vars.modifiedVars.BASE_HEALTH;
+                World.health = World.getMaxHealth();
+
+                Notifications.printMessage("Base health has been reset to original value.");
+            } else {
+                CheatMain.vars.modifiedVars.BASE_HEALTH = World.BASE_HEALTH;
+
+                World.BASE_HEALTH = CheatMain.vars.amountOfBaseHP;
+                World.health = World.getMaxHealth();
+
+                Notifications.printMessage("Base health has been set to: "+CheatMain.vars.amountOfBaseHP+".");
+            }
+        }
     },
     adrLotsOfStorage: function() {
-        return;
+        if (CheatMain.adrDustPathUnlocked()) {
+            if (CheatMain.vars.modifiedVars.amountOfBagSpace != Path.DEFAULT_BAG_SPACE && CheatMain.vars.modifiedVars.amountOfBagSpace < CheatMain.vars.amountOfBagSpace && CheatMain.vars.modifiedVars.amountOfBagSpace != 0) {
+                Path.DEFAULT_BAG_SPACE = CheatMain.vars.modifiedVars.amountOfBagSpace;
+
+                Notifications.printMessage("Default bag space reset to original value.");
+            } else {
+                CheatMain.vars.modifiedVars.amountOfBagSpace = Path.DEFAULT_BAG_SPACE;
+
+                Path.DEFAULT_BAG_SPACE = CheatMain.vars.amountOfBagSpace;
+
+                Notifications.printMessage("Default bag space set to: "+CheatMain.vars.amountOfBagSpace+".");
+            }
+        }
     },
     adrAddMeds: function() {
-        return;
+        if (CheatMain.adrDustPathUnlocked()) {
+            Path.outfit["cured meat"] = CheatMain.amountOfMedsToAdd;
+            Path.outfit["medicine"] = CheatMain.amountOfMedsToAdd;
+
+            Notifications.printMessage('Added '+CheatMain.amountToAdd+' of Cured Meat & Medicine to rucksack.');
+        }
     },
     adrResetDeathCooldown: function() {
-        return;
+        if (CheatMain.adrDustPathUnlocked()) {
+            Button.clearCooldown($('#embarkButton'));
+            Notifications.printMessage('Death Cooldown reset.');
+        }
+    },
+    adrAutoUpgradeShip: function() {
+        if ($SM.get('features.location.spaceShip')) {
+            if (!CheatMain.vars.automate.ship) {
+                CheatMain.vars.shipInterval = setInterval(function() {
+                    if ('alien alloy' in $SM.get('stores')) {
+                        reinforceButton.click();
+                        engineButton.click();
+                    }
+                }, 10);
+
+                CheatMain.vars.automate.ship = true;
+                Notifications.printMessage("Auto Reinforce and Upgrade for ship enabled.");
+            } else if (CheatMain.vars.automate.ship) {
+                clearInterval(CheatMain.vars.shipInterval);
+
+                CheatMain.vars.automate.ship = false;
+                Notifications.printMessage("Auto Reinforce and Upgrade for ship disabled.");
+            }
+        } else {
+            Events.startEvent({
+                title: _('Ship Not Unlocked'),
+                scenes: {
+                    start: {
+                        text: [_('You need to find the ship on the world map.')],
+                        buttons: {
+                            'okay': {
+                                text: _('Okay'),
+                                nextScene: 'end'
+                            }
+                        }
+                    }
+                }
+            });
+        }
     },
     buildCheatMenu: function() {
+        // build cheat menu
         var cMenu = $('<div>')
             .addClass('cMenu')
             .text('==Cheat Menu== | ')
@@ -152,6 +301,11 @@ var CheatMain = {
             .text(_('Give All Resources'))
             .css('cursor', 'pointer')
             .click(() => CheatMain.adrGiveResources())
+            .appendTo(cMenu);
+        $("<button>")
+            .text(_('Give All Weapons'))
+            .css('cursor', 'pointer')
+            .click(() => CheatMain.adrGiveWeapons())
             .appendTo(cMenu);
         $("<button>")
             .text(_('Give All Perks'))
@@ -183,10 +337,29 @@ var CheatMain = {
             .css('cursor', 'pointer')
             .click(() => CheatMain.adrResetDeathCooldown())
             .appendTo(cMenu);
+        $("<button>")
+            .text(_('Auto Upgrade Ship'))
+            .css('cursor', 'pointer')
+            .click(() => CheatMain.adrAutoUpgradeShip())
+            .appendTo(cMenu);
+
+        // style buttons
+        $("<style>")
+            .html(".cMenu button{position: relative; text-align: center; border: 1px solid black;margin-bottom: 5px;padding: 5px 10px;cursor: pointer;-webkit-touch-callout: none;-webkit-user-select: none;-khtml-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;} .cMenu button:hover{text-decoration:underline;} .cMenu.dark button{border:1px solid #EEE;background-color:#272823;color:#EEE;} .cMenu.dark{background-color:#272823;color:#EEE}")
+            .appendTo(document.head);
+
+        setInterval(function() {
+            // check for light off/light on
+            if ($SM.get("config.lightsOff")) {
+                cMenu.addClass('dark');
+            } else {
+                cMenu.removeClass('dark');
+            }
+        }, 100);
     }
 }
 
-
+// wait for page to be fully loaded
 if (document.readyState !== 'loading') {
     setTimeout(CheatMain.buildCheatMenu, 0);
 } else {
